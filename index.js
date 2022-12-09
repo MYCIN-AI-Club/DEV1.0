bde17f00933d5774be7a1cbefd7ec61a8f31a50e
prabhat_021 <prabhatsahrawat010203@gmail.com>
2022-12-09 23:25:02 +0530

    Responsivness added

---
+++ index.js
@@ -0,0 +1,10 @@
+var sideMenu = document.getElementsByClassName("nav-btn");
+
+function openMenu() {
+    sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+    sideMenu.style.right = "-200px";
+}
+
